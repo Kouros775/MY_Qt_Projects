dc7d02f4f90b81e1fd08ffa127b4e95f0cc472dc
DESKTOP-3NUIPCV\JMY <kouros775@naver.com>
2022-04-28 23:23:20 +0900

    Feat: QML Basic

--- QML/02_QML_Basic2/main.cpp
+++ QML/02_QML_Basic2/main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
+
 
 int main(int argc, char *argv[])
 {
@@ -10,9 +12,21 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     //engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     //engine.load(QUrl(QStringLiteral("qrc:/animatedimage.qml")));
-    engine.load(QUrl(QStringLiteral("qrc:/anchors.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/text_image_anchors.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/gradient.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/systempalette.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/screen.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/fontloader.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/repeater.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/image.qml")));
+    //engine.load(QUrl(QStringLiteral("qrc:/transformation.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/simple_accessible.qml")));
+
     if (engine.rootObjects().isEmpty())
+    {
+        qDebug() << __FUNCTION__;
         return -1;
+    }
 
     return app.exec();
 }
---
+++ QML/02_QML_Basic2/systempalette.qml
@@ -0,0 +1,30 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+
+Window
+{
+    visible: true;
+    width: 640;
+    height: 480;
+
+    Rectangle
+    {
+        width: 640;
+        height: 480;
+        color: myPalette.window;
+
+        SystemPalette
+        {
+            id: myPalette;
+            colorGroup: SystemPalette.Active;
+        }
+        Text
+        {
+            id: myText;
+            anchors.fill: parent;
+            text: "Hello!";
+            font.pixelSize: 32;
+            color: myPalette.windowText;
+        }
+    }
+}
